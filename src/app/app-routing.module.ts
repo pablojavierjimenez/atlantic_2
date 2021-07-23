dcc971d30e8cb47491989ca6fa6b13c806d6b7ef
Pablo javier jimenez <pablojavierjimenez@gmail.com>
2021-07-23 09:26:45 -0300

    finish separate a views on singles lazy modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./views/home/home.module').then((mod) => mod.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./views/about/about.module').then((mod) => mod.AboutModule),
+  },
+  {
+    path: 'not-found',
+    loadChildren: () =>
+      import('./views/not-found/not-found.module').then(
+        (mod) => mod.NotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
