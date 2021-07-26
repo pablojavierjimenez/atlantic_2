79e0a490e505b98015afe31d985479ea3d98e4ad
Pablo javier jimenez <pablojavierjimenez@gmail.com>
2021-07-26 07:46:23 -0300

    fix minor lint errors

--- e2e/src/puppeteer-test.e2e.spec.ts
+++ e2e/src/puppeteer-test.e2e.spec.ts
@@ -2,14 +2,14 @@ import 'expect-puppeteer';
 
 describe('Google', () => {
   beforeAll(async () => {
-    await page.goto('https://google.com')
+    await page.goto('https://google.com');
   });
 
   it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google')
+    await expect(page).toMatch('google');
   });
 
-  it("Puppeteer screenshot - from APP", async () => {
-    await page.screenshot({ path: "e2e/screenshot/index-home-screenshot.png" });
+  it('Puppeteer screenshot - from APP', async () => {
+    await page.screenshot({ path: 'e2e/screenshot/index-home-screenshot.png' });
   });
 });
